977ad588f017e3fe83e00b149175984a06d8bc2b
kanka@kishore <kankanalakishore9@gmail.com>
2023-05-08 17:58:04 -0400

    added comment in sum file and added new file of Multiplication table

--- src/main/java/day1/SumOfTwoNumbers.java
+++ src/main/java/day1/SumOfTwoNumbers.java
@@ -5,6 +5,7 @@ public class SumOfTwoNumbers {
         int a = 10;
         int b = 20;
         System.out.println(a+"+"+b+"="+(a+b));
+        System.out.println("this is my initial come");
     }
 }
   
