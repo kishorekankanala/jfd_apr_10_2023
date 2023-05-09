69f769faf552ef532a9e1198513517bdb3c4db20
kanka@kishore <kankanalakishore9@gmail.com>
2023-05-08 20:09:01 -0400

    modified

--- src/main/java/day1/FactorialOfNumber.java
+++ src/main/java/day1/FactorialOfNumber.java
@@ -1,8 +1,12 @@
 package day1;
 
 public class FactorialOfNumber {
-    public static void main(String[] args){
-        int a = 9;
-        System.out.println(a);
+    public static void main(String[] args) {
+        int num = 9;
+        int res = 1;
+        for (int i = 1; i <= num; i++) {
+            res = res * i;
+        }
+        System.out.println("factorial " + num + " is " + res);
     }
 }
